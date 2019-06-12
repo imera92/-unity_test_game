7dd0edab3880f2e1d0e883f8bb6c2211a8b88a30
Ivan Mera <imera92@gmail.com>
2019-06-12 07:54:17 -0500

    Implementación de ataque

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) {
+            animator.Play("skeleton_destroyed");
+            // Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        health -= damage;
+        animator.Play("skeleton_hit");
+        Debug.Log("DAMAGE TAKEN! CURRENT HEALTH: " + health);
+    }
+}
---
+++ Assets/scripts/PlayerAttack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private float time_btw_attack;
+    public float start_time_before_attack;
+
+    public Transform attack_position;
+    public LayerMask is_enemy;
+    public float attack_range;
+    public int damage;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.Play("player_attacking");
+            Collider2D[] damaged_enemies = Physics2D.OverlapCircleAll(attack_position.position, attack_range, is_enemy);
+            for (int i = 0; i < damaged_enemies.Length; i++)
+            {
+                damaged_enemies[i].GetComponent<Enemy>().TakeDamage(damage);
+            }
+        }
+        /*if (time_btw_attack <= 0) {
+            if (Input.GetKey(KeyCode.Z)) {
+                animator.Play("player_attacking");
+                Collider2D[] damaged_enemies = Physics2D.OverlapCircleAll(attack_position.position, attack_range, is_enemy);
+                for (int i = 0; i < damaged_enemies.Length; i++) {
+                    damaged_enemies[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            time_btw_attack = start_time_before_attack;
+        } else {
+            time_btw_attack -= Time.deltaTime;
+        }*/
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attack_position.position, attack_range);
+    }
+}
